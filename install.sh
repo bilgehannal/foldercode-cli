b9852ac405aa012bac9f3e8e828cede908b3b9a2
bilgehan.nal <bilgehan.nal@sahibinden.com>
2023-01-22 00:40:36 +0300

    install sh change

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo 'Downloading fcode executable file...'
-sudo wget "https://github.com/bilgehannal/foldercode-cli/releases/download/v0.0.1/fcode${$1}" -O /usr/local/bin/fcode
+sudo wget "https://github.com/bilgehannal/foldercode-cli/releases/download/v0.0.1/fcode.${1}" -O /usr/local/bin/fcode
 echo 'Checking permissions...'
 sudo chmod +x /usr/local/bin/fcode
 echo 'fcode is installed.'
