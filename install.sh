d1e6543bcd23c46fa4c453450faec8c743d255b3
bilgehan.nal <bilgehan.nal@sahibinden.com>
2023-01-21 22:22:29 +0300

    change READMEmd

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+echo 'Downloading fcode executable file...'
 sudo curl https://github.com/bilgehannal/foldercode-cli/releases/download/v0.0.1/fcode -o /usr/local/bin/fcode
+echo 'Checking permissions...'
 sudo chmod +x /usr/local/bin/fcode
+echo 'fcode is installed.'
+echo 'Run: fcode --help'
