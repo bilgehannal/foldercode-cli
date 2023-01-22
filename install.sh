fcf6f18a461cd2a1a2bad4586dd0a07a4c60b159
emrullah.cirit <emrullah.cirit@sahibinden.com>
2023-01-22 22:24:17 +0300

    get latest version of app

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 echo 'Downloading and installing fcode executable file...'
 
 BASE_RELEASES_URL="https://github.com/bilgehannal/foldercode-cli/releases/download"
-VERSION="$1"
-DISTRO="$2"
-ARCH="$3"
+VERSION=$(curl https://api.github.com/repos/bilgehannal/foldercode-cli/releases/latest|grep '"tag_name": '|cut -d 'v' -f 2|cut -d '"' -f 1)
+DISTRO="$1"
+ARCH="$2"
 TAR_NAME="foldercode-cli_${VERSION}_${DISTRO}_${ARCH}.tar.gz"
 wget "$BASE_RELEASES_URL/v${VERSION}/${TAR_NAME}"
 tar -xzvf $TAR_NAME fcode
